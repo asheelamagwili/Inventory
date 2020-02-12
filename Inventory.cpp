97c3e9a420ad1f9567e3a147ae8e496f7863ab97
Asheela Magwili <asheelamagwili@Asheelas-MacBook-Pro.local>
2020-02-12 11:22:55 -0800

    Documentation is added in-line

--- Inventory.cpp
+++ Inventory.cpp
@@ -7,6 +7,7 @@ using std::ostream;
 using std::cout;
 using std::endl;
   
+// Inventory class: name of item, price, amount
 Inventory::Inventory(string name, float price, int count)
 {
   m_name = name;
@@ -14,13 +15,16 @@ Inventory::Inventory(string name, float price, int count)
   m_in_stock = count;
 }
 
+// When user chooses to make a sale
 void Inventory::sell()
 {
+  // Substract one from invetory after a sale.
   if(m_in_stock > 0)
   {
     m_in_stock--;
   }
 
+  // When no inventory is left to sell, send error.
   else
   {
     cout<<"Sorry, that item is out of stock\n"<<endl;
--- main.cpp
+++ main.cpp
@@ -8,12 +8,14 @@ using std::string;
 int main()
 {
   char choice = 'q';
+  // Initial amount of the stock: Name of stock, price, amount
   Inventory stock("Cupcake",2.99,100);
 
   do
   {
     cout<<"\nMENU\n====\n(S)ell item\n(Q)uit\n>";
     cin>>choice;
+    // Take user input for Sell until user chooses to Quit
     if( choice == 'S' || choice == 's' )
     {
       stock.sell();
